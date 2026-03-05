ca94f4fd601b4db98e9ec18fed0412f2c7bbb797
adberrywb-art <adberrywb@gmail.com>
2026-03-05 11:40:58 -0600

    Delete inst.bat

--- inst.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo === VIP SIGNALS - INSTALANDO E INICIANDO ===
-echo.
-echo Ejecutando npm install con la ruta directa...
-"C:\Program Files\nodejs\npm.cmd" install
-echo.
-echo Instalacion completa. Iniciando servidor...
-"C:\Program Files\nodejs\npm.cmd" start
-pause
